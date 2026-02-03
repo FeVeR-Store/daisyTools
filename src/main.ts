6ad4fa500e730db127805dc0872f1ae5ec89acf3
FeVeR-Store <renyis@foxmail.com>
2026-02-03 14:09:29 +0800

    feat(frontend): update frontend code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "./main.css";
+import { router } from "./pages/pages";
+import "./inject";
+import { createI18n } from "vue-i18n";
 
-createApp(App).mount("#app");
+const i18n = createI18n({
+  legacy: false,
+  locale: "zh-CN",
+  fallbackLocale: {
+    "zh-CN": ["zh_cn"],
+  },
+});
+
+createApp(App).use(router).use(i18n).mount("#app");
