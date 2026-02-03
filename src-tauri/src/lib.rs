9ed14edb9f6901a01ae5dcbd8ee726b1b4a84c85
FeVeR-Store <renyis@foxmail.com>
2026-02-03 14:10:57 +0800

    feat(tauri): update tauri code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,71 @@
+use application::command::{get_config, open_window, save_config};
+// use pipe::client::communicate_with_service;
+use service::{
+    action::command::{
+        get_lit_action, register_action, remove_action, run_action_by_id, update_action_plug,
+    },
+    status::{get_service_state, get_service_state_file, launch_service},
+    task::create_task,
+    trigger::{
+        command::{get_lit_trigger, register_trigger, remove_trigger},
+        time::is_cron_expression_vaild,
+    },
+};
+use tauri::{webview, LogicalPosition, LogicalSize};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
+pub mod application;
+pub mod elevation;
+// pub mod ipc;
+pub mod runtime;
+pub mod service;
+pub mod utils;
+// #[tauri::command]
+// fn call_service() {
+//     communicate_with_service().unwrap();
+// }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_fs::init())
+        .setup(|app| {
+            let width = 800.;
+            let height = 600.;
+            let window = tauri::window::WindowBuilder::new(app, "main")
+                .inner_size(width, height)
+                .decorations(false)
+                .build()?;
+            window.add_child(
+                webview::WebviewBuilder::new("main", tauri::WebviewUrl::App(Default::default()))
+                    .auto_resize(),
+                LogicalPosition::new(0., 0.),
+                LogicalSize::new(width, height),
+            )?;
+            Ok(())
+        })
+        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            register_action,
+            run_action_by_id,
+            get_lit_trigger,
+            register_trigger,
+            get_lit_action,
+            get_service_state,
+            launch_service,
+            get_service_state_file,
+            create_task,
+            remove_action,
+            remove_trigger,
+            is_cron_expression_vaild,
+            update_action_plug,
+            save_config,
+            get_config,
+            open_window
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,122 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{
+    env::set_var,
+    fs::{create_dir, exists},
+};
+
+use clap::Parser;
+use daisytools_lib::{
+    elevation,
+    runtime::javascript::execute_javascript_from_tauri,
+    service::{
+        install_service, launch_service, start_service, status::query_service_status,
+        unintall_service,
+    },
+};
+use ftail::Ftail;
+use log::LevelFilter;
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+#[derive(Debug, clap::Subcommand)]
+enum Command {
+    Service {
+        #[command(subcommand)]
+        action: Option<ServiceCommand>,
+    },
+    Task {
+        #[command(subcommand)]
+        action: TaskCommand,
+    },
+}
+
+#[derive(Debug, clap::Subcommand)]
+enum ServiceCommand {
+    Launch,
+    Install,
+    Uninstall,
+}
+
+#[derive(Debug, clap::Subcommand)]
+enum TaskCommand {
+    Run { code: String },
+}
+
 fn main() {
-    daisytools_lib::run()
+    #[cfg(debug_assertions)]
+    set_var("RUST_BACKTRACE", "1");
+    let cli = Args::parse();
+    if !exists("logs").unwrap() {
+        create_dir("logs").unwrap()
+    }
+    let Some(cmd) = cli.command else {
+        // 如果可以查询到状态，说明已安装服务
+        // 如果没有安装服务，则判断是否授予管理员权限
+        if query_service_status().is_ok() || privilege::user::privileged() {
+            Ftail::new()
+                .single_file("logs/client.log", true, LevelFilter::Off)
+                .init()
+                .unwrap();
+            // 启动UI
+            return daisytools_lib::run();
+        };
+        // 若未安装服务且没有权限，则请求权限
+        elevation::request_elevation(None).unwrap();
+        return;
+    };
+    // cli
+    Ftail::new()
+        .single_file(
+            "D:\\codes\\daisyTools\\src-tauri\\logs\\service.log",
+            true,
+            LevelFilter::Off,
+        )
+        .init()
+        .unwrap();
+    let res = match cmd {
+        // 服务相关
+        Command::Service {
+            action: Some(action),
+        } => {
+            if privilege::user::privileged() {
+                // 下面的内容需要管理员权限
+                match action {
+                    ServiceCommand::Launch => launch_service(),
+                    ServiceCommand::Install => install_service(),
+                    ServiceCommand::Uninstall => unintall_service(),
+                }
+            } else {
+                let arg = match action {
+                    ServiceCommand::Launch => "launch",
+                    ServiceCommand::Install => "install",
+                    ServiceCommand::Uninstall => "unintall",
+                };
+                // 若没有权限，则请求权限
+                elevation::request_elevation(Some(&("service ".to_string() + arg))).unwrap();
+                return;
+            }
+        }
+        Command::Service { action: None } => {
+            log::info!("start service");
+            start_service()
+        }
+        Command::Task { action } => match action {
+            TaskCommand::Run { code: action_id } => {
+                execute_javascript_from_tauri(&action_id).unwrap();
+                return;
+            }
+        },
+    };
+    match res {
+        Err(e) => panic!("{} ", e),
+        Ok(()) => (),
+    }
 }
